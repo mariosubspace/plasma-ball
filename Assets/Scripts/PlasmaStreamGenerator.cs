c0ec70d5809974861cac83c716900af645f1cc33
Mario Gutierrez <mariosubspace@gmail.com>
2018-11-14 00:51:18 -0600

    Plasma ball done for now...

--- Assets/Scripts/PlasmaStreamGenerator.cs
+++ Assets/Scripts/PlasmaStreamGenerator.cs
@@ -8,17 +8,38 @@ public class PlasmaStreamGenerator : MonoBehaviour
 	new ParticleSystem particleSystem;
 	List<LineRenderer> lines = new List<LineRenderer>();
 
+	public Mesh centerCap;
+
 	[Header("Line")]
 	public AnimationCurve lineWidthCurve;
 	public Material lineMaterial;
+	[GradientHDR]
 	public Gradient lineColor;
 	public int subdivisionCount = 8;
 	public float noiseAmplitude = 0.1f;
 	public float noiseScale = 1f;
+	public float noiseFrequency = 1f;
 
 	void Awake()
 	{
-		particleSystem = GetComponent<ParticleSystem>();
+		// Create a particle system, will be used to randomly generate
+		// spawn points.
+		particleSystem = gameObject.AddComponent<ParticleSystem>();
+
+		// Set up emission shape.
+		ParticleSystem.ShapeModule pshape = particleSystem.shape;
+		pshape.shapeType = ParticleSystemShapeType.Mesh;
+		pshape.mesh = centerCap;
+		pshape.rotation = new Vector3(-90, 0, 0);
+		pshape.radiusThickness = 0;
+
+		// Set up the main particle system settings.
+		ParticleSystem.MainModule pmain = particleSystem.main;
+		pmain.startSpeed = 0;
+		pmain.startLifetime = new ParticleSystem.MinMaxCurve(0.5f, 2f);
+
+		// Disable particle system renderer.
+		particleSystem.gameObject.GetComponent<ParticleSystemRenderer>().enabled = false;
 	}
 
 	void Update()
@@ -27,6 +48,7 @@ public class PlasmaStreamGenerator : MonoBehaviour
 		ParticleSystem.Particle[] particles = new ParticleSystem.Particle[particleSystem.particleCount];
 		particleSystem.GetParticles(particles);
 
+		// Add additional plasma lines if there are less than the amount of particles.
 		while (lines.Count < particles.Length)
 		{
 			GameObject gob = new GameObject();
@@ -35,7 +57,21 @@ public class PlasmaStreamGenerator : MonoBehaviour
 			line.widthCurve = lineWidthCurve;
 			line.material = lineMaterial;
 			line.colorGradient = lineColor;
+			line.numCapVertices = 8;
+			line.numCornerVertices = 8;
 			lines.Add(line);
+		}
+
+		// Remove superfluous lines (really inefficient, but not really important
+		// to make it efficient right now...)
+		{
+			int li = lines.Count - 1;
+			while (lines.Count > particles.Length)
+			{
+				Destroy(lines[li].gameObject);
+				lines.RemoveAt(li);
+				--li;
+			}
 		}
 
 		// Project point onto sphere from center to outer sphere (B).
@@ -52,11 +88,15 @@ public class PlasmaStreamGenerator : MonoBehaviour
 			for (int j = 0; j <= subdivisionCount; ++j)
 			{
 				Vector3 sPos = Vector3.Lerp(A, B, ((float)j) / subdivisionCount);
-				float seed = Time.time + i + j * noiseScale;
-				sPos += new Vector3 (
+				float seed = Time.time * noiseFrequency + i + j * noiseScale;
+				// Add noise if it's not the start or end-point.
+				if (j != 0 && j != subdivisionCount)
+				{
+					sPos += new Vector3 (
 					Mathf.PerlinNoise(seed, 0) * 2f- 1f,
 					Mathf.PerlinNoise(seed + 1, 0) * 2f - 1f,
 					Mathf.PerlinNoise(seed + 2, 0) * 2f - 1f) * noiseAmplitude;
+				}
 				subdivisions[j] = sPos;
 			}
 			line.SetPositions(subdivisions);
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mario
+{
+	public class Rotator : MonoBehaviour
+	{
+		public float speed = 1f;
+
+		void Update()
+		{
+			transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0), Space.World);
+		}
+	}
+}
